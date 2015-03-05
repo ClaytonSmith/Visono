043d35ea5b648449675a86fd28ae297810729abe
ClaytonSmith <Cks_jr@yahoo.com>
2015-03-05 01:27:23 -0500

    Implemented drag and drop functionality

--- public/js/app.js
+++ public/js/app.js
@@ -8,8 +8,8 @@
 
 'use strict';
 
-// Declare app level module which depends on filters, and services
-var app = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives','ngRoute', 'flow']);
+// Declare app level module which depends on filters, and services                                          
+var app = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives','ngRoute', 'angularFileUpload', 'ngResource']);
 
 // Configure angular client side routing
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
@@ -26,22 +26,23 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
     $locationProvider.html5Mode(true);
 }]);
 
-app.config(['flowFactoryProvider', function (flowFactoryProvider) {
-    flowFactoryProvider.defaults = {
+/*app.config(['flowFactoryProvider', function (flowFactoryProvider) {
+   flowFactoryProvider.defaults = {
 	target: '',
 	permanentErrors: [500, 501],
 	maxChunkRetries: 1,
 	chunkRetryInterval: 5000,
 	simultaneousUploads: 1
-    };
+    };        
     
-    flowFactoryProvider.on('catchAll', function (event) {
-	console.log('catchAll', arguments);
-    });
+    // flowFactoryProvider.on('catchAll', function (event) {
+    // 	console.log('THIS IS A THIGN');
+    // 	console.log('catchAll', arguments);
+    // });
     
     // Can be used with different implementations of Flow.js
     flowFactoryProvider.factory = fustyFlowFactory;
-}]);
+}]); */
 
 
 
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,12 +2,13 @@
 
 /* Controllers */
 
-function AppCtrl($scope, $http) {
+function AppCtrl($scope, $http, $location) {
     $http({method: 'GET', url: '/api/name'}).
-	success(function(data, status, headers, config) {
-	    console.log(data);
-	    $scope.name = data.name;
-	}).
+	
+    success(function(data, status, headers, config) {
+	console.log(data);
+	$scope.name = data.name;
+    }).
 	error(function(data, status, headers, config) {
 	    $scope.name = 'Error!'
 	});
@@ -17,24 +18,63 @@ function AppCtrl($scope, $http) {
     };
 }
 
-function homeCtrl($scope, $http) {}
-function uploadCtrl($scope, $http){}
-function settingsCtrl($scope, $http){}
-function viewerCtrl($scope, $http){}
-function galleryCtrl($scope, $http){}
-function userCtrl($scope, $http){}
-function aboutCtrl($scope, $http){}
+function homeCtrl($scope, $http, $location, $upload) {
+    
+    // TEMP VALUE, CHANGE WHEN NOT IN DEV MODE.
+    $scope.test = false;
+    
+    $scope.changeView = function(){
+	$location.path('/settings');
+    }
+    
+    $scope.$watch('files', function () {
+	$scope.upload($scope.files);
+    });
+    
+    $scope.upload = function (files) {
+	if (files && files.length) {
+	console.log('File has been given');
+	    $scope.test = true;
+	 
+	// Working upload 
+	/* if (files && files.length) {
+           for (var i = 0; i < files.length; i++) {
+           var file = files[i];
+           $upload.upload({
+           url: 'upload/url',
+           fields: {'username': $scope.username},
+           file: file
+           }).progress(function (evt) {
+           var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+           console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+           }).success(function (data, status, headers, config) {
+           console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+           });
+           }
+           }*/
+	}
+    };
+}
+
+function settingsCtrl($scope, $http, $location){}
+
+function uploadCtrl($scope, $http, $location){}
+
+function viewerCtrl($scope, $http, $location){}
+function galleryCtrl($scope, $http, $location){}
+function userCtrl($scope, $http, $location){}
+function aboutCtrl($scope, $http, $location){}
 
 //DO NOT TOUCH 
 // unless you are adding or removing a controller
 
-settingsCtrl. $inject  =  ['$scope', '$http']; 
-galleryCtrl.  $inject  =  ['$scope', '$http'];
-uploadCtrl.   $inject  =  ['$scope', '$http'];
-viewerCtrl.   $inject  =  ['$scope', '$http'];
-aboutCtrl.    $inject  =  ['$scope', '$http'];
-homeCtrl.     $inject  =  ['$scope', '$http'];
-userCtrl.     $inject  =  ['$scope', '$http'];
+settingsCtrl. $inject  =  ['$scope', '$http', '$location']; 
+galleryCtrl.  $inject  =  ['$scope', '$http', '$location'];
+uploadCtrl.   $inject  =  ['$scope', '$http', '$location'];
+viewerCtrl.   $inject  =  ['$scope', '$http', '$location'];
+aboutCtrl.    $inject  =  ['$scope', '$http', '$location'];
+homeCtrl.     $inject  =  ['$scope', '$http', '$location', '$upload'];
+userCtrl.     $inject  =  ['$scope', '$http', '$location'];
 
 
 
